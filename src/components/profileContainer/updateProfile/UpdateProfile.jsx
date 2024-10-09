4db162e3472fa94d4d95b33e2b1ef9d53639a5b2
jellybeans macbook pro <password1234@passwords-MacBook-Pro.local>
2024-10-09 12:32:51 +0100

    chore: add update profile function

---
+++ src/components/profileContainer/updateProfile/UpdateProfile.jsx
@@ -0,0 +1,60 @@
+// import React, {useState} from 'react';
+
+// const UpdateProfile = () => {
+//     const [UpdateProfile, setUpdateProfile] = useState({
+//         name: '',
+//         email: '',
+//         password: ''
+    
+//     });
+
+//     const handleChange = (e) => {
+//         setUpdateProfile({
+//             ...Profiler,
+//             [e.target.name]: e.target.value
+//         });
+//     };
+
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//         // Handle the save/update logic here(like sending data to a server)
+//         console.log("Updated Profile:", profile);
+//         alert('profile updated successfully!');
+//     };
+
+//     return (
+//         <div>
+//         <h2>Update Profile</h2>
+//         <form onSubmit={handleSubmit}>
+//         <div>
+//         <label>Name:</label>
+//         <input
+//         type="text"
+//         name="name"
+//         value={profile.name}
+//         onChange={handleChange}
+//         />
+//         </div>
+//         <div>
+//             <label>Email:</label>
+//             <input 
+//             type="email"
+//             name="email"
+//             value={profile.email}
+//             onChange={handleChange}
+//             />
+//         </div>
+//         <div>
+//             <label>Password:</label>
+//             <input
+//             type="password"
+//             name="pasword"
+//             value={profile.password}
+//             onChange={handleChange}
+//             />
+//         </div>
+//         <button type="submit">Save Changes</button>
+//         </form>
+// </div>
+//     );
+// };
