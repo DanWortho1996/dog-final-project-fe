08048378f959686f9ec10506f2a35f0fb74b03c3
Samm Wiltshire <samm94tw@gmail.com>
2024-10-09 11:18:24 +0100

    enhancement: header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="header">
       <div className="logo">
-        <h1>YourLogo</h1>
+        <h1>PawPals</h1>
       </div>
       <nav className="nav">
         <ul>
