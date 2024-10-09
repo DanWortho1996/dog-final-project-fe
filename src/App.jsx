4f40d484c8a24d678385843dfd12e5d4de9d6be9
Daniel Worthington <daniel.worthington96@outlook.com>
2024-10-09 15:42:20 +0100

    enhancment: display flex

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css';
-import Header from './src/header/Header.jsx';
-import Footer from './src/footer/Footer.jsx';
+import Header from './header/Header.jsx';
+import Footer from './footer/Footer.jsx';
 
 function App() {
 
   return (
     <div className="logo-container">
-        <img src="/src/assets/Instadog.png" alt="Instadog Logo" className="logo" />
+        <img src="./images/logo1.png" alt="PawPals Logo" className="logo" />
       </div>
   )
 }
