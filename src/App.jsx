ea7a111c290a0922e62ce9872fc66d8f46eaf10f
Samm Wiltshire <samm94tw@gmail.com>
2024-10-10 14:00:40 +0100

    enhancement: bugfix

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,16 @@ import MenuPage from './pages/menuPage/MenuPage';
 // import ProfilePage from './pages/profilePage/ProfilePage';
 // import UserListPage from './pages/userListPage/UserListPage';
 import LandingPage from './pages/landingPage/LandingPage';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="logo-container">
-        <img src="/src/components/images/logo1.png" alt="logo1 Logo" className="logo1" />       
             <Router>
-              <Switch>
-              <Route path="/" components={<LandingPage/>} />
-              <Route path="/menu" components={<MenuPage />} />
-              {/* <Route path="/" element={<LandingPage/>} />
-              <Route path="/menu" element={<MenuPage />} /> */}
-              </Switch>
+              <Routes>
+              <Route path="/" element={<LandingPage/>} />
+              <Route path="/menu" element={<MenuPage />} />
+              </Routes>
             </Router>
-            </div>
   )
 };
 
