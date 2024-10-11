b0318cd639f6b1d8d3b0f41389810649021f7833
Samm Wiltshire <samm94tw@gmail.com>
2024-10-11 10:48:49 +0100

    logout added

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
 import './App.css';
-
-import Layout from './components/layout/Layout'
+import Layout from './components/layout/Layout';
 import MenuPage from './pages/menuPage/MenuPage';
+import LandingPage from './pages/landingPage/LandingPage';
 // import ProfilePage from './pages/profilePage/ProfilePage';
 // import UserListPage from './pages/userListPage/UserListPage';
-import LandingPage from './pages/landingPage/LandingPage';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
+  const handleLogout = () => {
+    localStorage.removeItem('user');
+    window.location.href = '/login';
+  };
+
   return (
-           <Layout>
-             <Router>
-              <Routes>
-              {/* <Route path="/" element={<ProfilePage/>} />
-              <Route path="/menu" element={<UserListPage />} /> */}
-              <Route path="/" element={<LandingPage/>} />
-              <Route path="/menu" element={<MenuPage />} />
-              </Routes>
-            </Router>
-           </Layout>
-  )
-};
+    <Router>
+      <Layout>
+        {/* Include the Header and pass the logout function */}
+        <Header onLogout={handleLogout} />
+
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/menu" element={<MenuPage />} />
+          {/* <Route path="/profile" element={<ProfilePage />} /> */}
+          {/* <Route path="/userlist" element={<UserListPage />} /> */}
+        </Routes>
+      </Layout>
+    </Router>
+  );
+}
 
 export default App;
