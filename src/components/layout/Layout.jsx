3326a5d4a01243aaa8bafd205a24decca4147b1e
Daniel Worthington <daniel.worthington96@outlook.com>
2024-10-14 14:30:55 +0100

    chore: Outlet / Link

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,5 +1,6 @@
 import Header from "../header/Header";
 import Footer from "../footer/Footer";
+import { Outlet } from "react-router-dom";
 
 import "./Layout.css";
 
@@ -9,6 +10,7 @@ const Layout = ({ children }) => {
       <Header />
       <main className="main-content">
         {children}
+        <Outlet />
       </main>
       <Footer />
     </div>
