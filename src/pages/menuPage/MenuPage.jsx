86a91c6c149cff98e7c203b3ed1ad17289b32449
Michael Hampson <mdavidhampson@gmail.com>
2024-10-14 14:34:01 +0100

    enchancement:add dog of the day api

--- src/pages/menuPage/MenuPage.jsx
+++ src/pages/menuPage/MenuPage.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import {useNavigate} from 'react-router-dom';
 import "./MenuPage.css";
+import DogBreedsMenu from '../../components/menuContainer/MenuContainer';
 
 const MenuPage = () => {
   const navigate = useNavigate();
   return (
+    <div>
     <div className="box-container">
       <button className="boxOne" onClick={() => navigate("/userlist")}>Active Users</button>
       <button className="boxTwo">Dog Events</button>
       <button className="boxThree">Update Profile</button>
       <button className="boxFour">HELP</button>
     </div>
+    <div className="dog-breeds-container">
+    <DogBreedsMenu />  {/* Adding the new component centered below existing buttons */}
+  </div>
+</div>
   )
 };
 
