606d1dc1e14e2c1a09d601206d12d37ae12745e4
Michael Hampson <mdavidhampson@gmail.com>
2024-10-16 11:02:38 +0100

    enhancement: populate eventpage

--- src/pages/dogEventPage/DogEventsPage.jsx
+++ src/pages/dogEventPage/DogEventsPage.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
+
 const DogEventsPage = () => {
   return (
-    <div>DogEventsPage</div>
+    <div className="container">
+      <div className="header">Paw-Community Events</div>
+      <div className="ad-box">
+        <div className="ad-placeholder"><img src="../../../images/fakeadvert1.png" alt = "dog event ad" height= "350px" width = "auto"></img> </div>
+      </div>
+      <div className="ad-box">
+        <div className="ad-placeholder">placeholder</div>
+      </div>
+      <div className="ad-box">
+        <div className="ad-placeholder">placeholder</div>
+      </div>
+      <div className="description">
+        <p>Our community events bring everyone together by fostering relationships, sharing resources, and creating a sense of belonging. Whether you're participating in fun activities or simply enjoying the company of others, we strive to make everyone feel welcome and connected.</p>
+      </div>
+    </div>
   )
 }
 
