3d8376f1507f22fdae17e9b8a4bfb10b4846071b
Samm Wiltshire <samm94tw@gmail.com>
2024-10-17 11:46:09 +0100

    enhancement: profilePage shows details once user logged in, however need backend login

--- src/pages/profilePage/ProfilePage.jsx
+++ src/pages/profilePage/ProfilePage.jsx
@@ -1,38 +1,35 @@
-import ProfileContainer from "../../components/profileContainer/ProfileContainer";
+import ProfileContainer from "../../components/profileContainer/ProfileContainer"; 
 import React from 'react';
-
 
 const ProfilePage = ({ isLoggedIn, loggedUser, setIsLoggedIn, setLoggedUser }) => {
 
+  if (!isLoggedIn || !loggedUser) {
+    return <p>Please log in to view your profile.</p>;
+  }
 
-  return (
-    <div className="profile-delete">
-      
-      <ProfileContainer loggedUser={loggedUser} setIsLoggedIn={setIsLoggedIn} setLoggedUser={setLoggedUser}/>
-    </div>
+  const { username, email } = loggedUser.user; 
+
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+
+    setIsLoggedIn(true);
+    setLoggedUser(userData);
     
+    navigate('/profilepage');
+  };
+  return (
+    <div className="profile-page">
+      <h1>Welcome to your profile, {username}!</h1>
+      <div className="profile-details">
+        <p><strong>Username:</strong> {username}</p>
+        <p><strong>Email:</strong> {email}</p>
+        {/* Add more user info */}
+      </div>
+      <ProfileContainer loggedUser={loggedUser} setIsLoggedIn={setIsLoggedIn} setLoggedUser={setLoggedUser} 
+      />
+    </div>
   );
 };
 
 export default ProfilePage;
-
-
-//   if (!isLoggedIn || !loggedUser) {
-//     return <p>Please log in to view your profile.</p>;
-//   }
-
-//   // Destructure user details from the loggedUser object
-//   const { username, email } = loggedUser.user; 
-
-//   return (
-//     <div className="profile-page">
-//       <h1>Welcome to your profile, {username}!</h1>
-//       <div className="profile-details">
-//         <p><strong>Username:</strong> {username}</p>
-//         <p><strong>Email:</strong> {email}</p>
-//         {/* Add other user information as needed */}
-//       </div>
-//     </div>
-//   );
-// };
-
