ac6945808ae1e42946d1ddc08975a7a7d7152729
Daniel Worthington <daniel.worthington96@outlook.com>
2024-10-17 17:29:09 +0100

    enhancement: google maps

--- src/components/userListContainer/UserListContainer.jsx
+++ src/components/userListContainer/UserListContainer.jsx
@@ -1,9 +1,40 @@
-import {useEffect, useState} from "react";
+import  React, {useEffect, useState} from "react";
 import { allUsersFetch } from "../../utils/fetch";
 import "./UserListContainer.css";
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+
+const containerStyle = {
+    width: '100vh',
+    height: '80vh',
+  }
+  
+  const center = {
+    lat: 53.4084,
+    lng: -2.9916,
+  }
+
 
 const UserListContainer = ({isLoggedIn}) => {
     const [users, setUsers] = useState([]);
+
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API,
+      })
+    
+      const [map, setMap] = React.useState(null)
+    
+      const onLoad = React.useCallback(function callback(map) {
+        // This is just an example of getting and using the map instance!!! don't just blindly copy!
+        const bounds = new window.google.maps.LatLngBounds(center)
+        map.fitBounds(bounds)
+    
+        setMap(map)
+      }, [])
+    
+      const onUnmount = React.useCallback(function callback(map) {
+        setMap(null)
+      }, [])
 
     useEffect(() => {
         console.log("useEffect in UserListContainer");
@@ -30,6 +61,15 @@ const UserListContainer = ({isLoggedIn}) => {
                 ) : (
                     <p>No Users</p>
                 )}
+            </div>
+            <div>
+                {isLoaded ?  <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    /> : null}
             </div>
         </div>
     );
