2890ad2bdd839fcdd73732c2d62740bff933675a
jellybeans macbook pro <password1234@passwords-MacBook-Pro.local>
2024-10-18 11:46:24 +0100

    bug

--- src/components/profileContainer/ProfileContainer.jsx
+++ src/components/profileContainer/ProfileContainer.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes} from 'react-router-dom'
 import ProfileDelete from "./profileDelete/ProfileDelete"
 import ProfileDisplay from "./profileDisplay/ProfileDisplay"
-import UpdateProfile from "./updateprofile/Updateprofile"
+import UpdateProfile from './updateProfile/UpdateProfile'
 
 const ProfileContainer = ({ isLoggedIn, loggedUser, setIsLoggedIn, setLoggedUser }) => {
   return (
